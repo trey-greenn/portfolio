05db4d777894cfb40217141ab3c4a8a7743c528f
trey-greenn <w.treygreen@gmail.com>
2023-10-25 08:50:13 -0700

    update gtc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,24 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Link from 'next/link';
+import React from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
+
 export default function Home() {
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('/api/data')
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <Image
@@ -42,77 +29,104 @@ export default function Home() {
           height={37}
           priority
         />
+        {/* {data.map((dat :any, index) => (
+          <p>{dat.name}</p>
+        ))} */}
+        
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+      <div className="rounded-lg border-white mt-12"> 
+      
+      <div className="port_website grid grid-cols-2 border border-gray-600 rounded-lg p-4 ring-inset ring bg-gray-900">
+        <Image
+          src="/Untitled.png"
+          alt="Guess That Clue"
+          width={180}
+          height={48}
+        
+        />
+        <Link href="https://mergacquilous.com ">
+          <h2 className="font-bold text-center">
+            Mergacquilous
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+          <p>  </p>
+          <p className="mt-6 mb-4 text-center">
+            You think you know business? If you were given data that painted the financial picture of a business. Would you be able to solve the riddle? Probably not. Want to give it a shot?
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <p>Tech Stack : Next.Js, Python, Mongo DB, Typescript, Tailwindcss, Vercel, </p>
+          
+        </Link>        
       </div>
+        
+      </div>
+      <div className="rounded-lg border-white mt-12"> 
+      
+      <div className="port_website grid grid-cols-2 border border-gray-600 rounded-lg p-4 ring-inset ring bg-gray-900">
+        <Image
+          src="/Untitled.png"
+          alt="Guess That Clue"
+          width={180}
+          height={48}
+        
+        />
+        <Link href="https://gtc-navy.vercel.app/ ">
+          <h2 className="font-bold text-center">
+            Guess That Clue
+          </h2>
+          <p className="mt-6 mb-4 text-center">
+            You think you know business? If you were given data that painted the financial picture of a business. Would you be able to solve the riddle? Probably not. Want to give it a shot?
+          </p>
+          <p>Tech Stack : Next.Js, GraphQL, Typescript, Tailwindcss, Vercel </p>
+        </Link>        
+      </div>
+        
+      </div>
+      <div className="rounded-lg border-white mt-12"> 
+      
+      <div className="port_website grid grid-cols-2 border border-gray-600 rounded-lg p-4 ring-inset ring bg-gray-900">
+        <Image
+          src="/Untitled.png"
+          alt="Guess That Clue"
+          width={180}
+          height={48}
+        
+        />
+        <Link href="https://theopenchess.com ">
+          <h2 className="font-bold text-center">
+            The Open Chess
+          </h2>
+          <p className="mt-6 mb-4 text-center">
+            You think you know business? If you were given data that painted the financial picture of a business. Would you be able to solve the riddle? Probably not. Want to give it a shot?
+          </p>
+          <p>Tech Stack : Next.Js, GraphQL, Typescript, Tailwindcss, Vercel, Shopify </p>
+        </Link>        
+      </div>
+        
+      </div>
+      <div className="rounded-lg border-white mt-12"> 
+      
+      <div className="port_website grid grid-cols-2 border border-gray-600 rounded-lg p-4 ring-inset ring bg-gray-900">
+        <Image
+          src="/Untitled.png"
+          alt="Guess That Clue"
+          width={180}
+          height={48}
+        
+        />
+        <Link href="https://gtc-navy.vercel.app/ ">
+          <h2 className="font-bold text-center">
+            Guess That Clue
+          </h2>
+          <p className="mt-6 mb-4 text-center">
+            You think you know business? If you were given data that painted the financial picture of a business. Would you be able to solve the riddle? Probably not. Want to give it a shot?
+          </p>
+          <p>Tech Stack : Next.Js, GraphQL, Typescript, Tailwindcss, Vercel </p>
+        </Link>        
+      </div>
+        
+      </div>
+      
+
+      
     </main>
   )
 }
