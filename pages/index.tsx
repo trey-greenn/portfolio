a625daa7677ab2108a12a8755509b3f8196c2dbe
trey-greenn <w.treygreen@gmail.com>
2023-10-25 11:40:32 -0700

    port update

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,17 +21,8 @@ export default function Home() {
       
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        {/* {data.map((dat :any, index) => (
-          <p>{dat.name}</p>
-        ))} */}
+        <h1 className="font-bold text-2xl">William Trey Green</h1>
+ 
         
       </div>
       <div className="rounded-lg border-white mt-12"> 
@@ -48,7 +39,7 @@ export default function Home() {
           <h2 className="font-bold text-center">
             Mergacquilous
           </h2>
-          <p>  </p>
+         
           <p className="mt-6 mb-4 text-center">
             You think you know business? If you were given data that painted the financial picture of a business. Would you be able to solve the riddle? Probably not. Want to give it a shot?
           </p>
