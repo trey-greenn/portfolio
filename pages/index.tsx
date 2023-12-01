2288ba425d41fee59911497c149f045c9476e4b8
Trey Green <treygreen@Treys-MBP.lan>
2023-12-01 12:33:53 -0500

    homepage update

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,22 +12,23 @@ export default function Home() {
     <main
     >
 
-      <div className="relative flex justify-center p-8 lace-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+      <div className="">
         <h1 className="font-bold text-2xl text-center">William Trey Green</h1>
  
         
       </div>
-      <div className="grid grid-cols-5"> 
+      <div className="grid md:grid-cols-9 mx-12 gap-2"> 
       
-      <div className="col-start-2 col-end-5 ">
+      {/* <div className="col-start-2 col-end-5 "> */}
 
-      
-      <div className="port_website grid grid-cols-2 rounded-lg p-4  bg-sky-900">
+      <div className="rounded-lg border-white mt-12 col-span-3 "> 
+
+      <div className="port_website grid grid-flow-row grid-flow-col rounded-lg bg-gradient-to-r from-sky-900 to-green-900 place-items-center">
         <Image
           src="/Untitled.png"
           alt="Guess That Clue"
-          width={180}
-          height={48}
+          width={84}
+          height={84}
         
         />
         <Link href="https://mergacquilous.com ">
@@ -35,62 +36,60 @@ export default function Home() {
             Mergacquilous
           </h2>
          
-          <p className="mt-6 mb-4 text-center">
+          <p className="text-center text-clip">
             You think you know business? If you were given data that painted the financial picture of a business. Would you be able to solve the riddle? Probably not. Want to give it a shot?
           </p>
-          <p>Tech Stack : Next.Js, Python, Mongo DB, Typescript, Tailwindcss, Vercel, </p>
           
         </Link>        
       </div>
+      </div>
         
       
-      <div className="rounded-lg border-white mt-12 col-start-2 col-end-5"> 
+      <div className="rounded-lg border-white mt-12 col-span-3 "> 
       
-      <div className="port_website grid grid-cols-2 rounded-lg p-4  bg-green-900">
+      <div className="port_website grid grid-flow-col grid-flow-row rounded-lg bg-green-900 place-items-center">
         <Image
           src="/Untitled.png"
           alt="Guess That Clue"
-          width={180}
-          height={48}
+          width={84}
+          height={84}
         
         />
         <Link href="https://gtc-navy.vercel.app/ ">
           <h2 className="font-bold text-center">
             Guess That Clue
           </h2>
-          <p className="mt-6 mb-4 text-center">
+          <p className="text-center">
             You think you know business? If you were given data that painted the financial picture of a business. Would you be able to solve the riddle? Probably not. Want to give it a shot?
           </p>
-          <p>Tech Stack : Next.Js, GraphQL, Typescript, Tailwindcss, Vercel </p>
         </Link>        
       </div>
         
       </div>
-      <div className="rounded-lg border-white mt-12 col-start-2 col-end-5"> 
+      <div className="rounded-lg border-white mt-12 col-span-3"> 
       
-      <div className="port_website grid grid-cols-2  rounded-lg p-4  bg-amber-900">
+      <div className="port_website grid grid-flow-col grid-flow-row rounded-lg bg-gradient-to-r from-green-900 bg-amber-900 place-items-center">
         <Image
           src="/Untitled.png"
           alt="Guess That Clue"
-          width={180}
-          height={48}
+          width={84}
+          height={84}
         
         />
         <Link href="https://theopenchess.com ">
-          <h2 className="font-bold text-center">
+          <h2 className="font-bold text-center ">
             The Open Chess
           </h2>
-          <p className="mt-6 mb-4 text-center">
+          <p className="text-center">
             You think you know business? If you were given data that painted the financial picture of a business. Would you be able to solve the riddle? Probably not. Want to give it a shot?
           </p>
-          <p>Tech Stack : Next.Js, GraphQL, Typescript, Tailwindcss, Vercel, Shopify </p>
         </Link>        
       </div>
         
       </div>
 
       
-      </div>
+      {/* </div> */}
       </div>
     </main>
   )
